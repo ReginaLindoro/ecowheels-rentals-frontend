d75894a176ade735584c0a05485d7d0f5fbc6aef
WeintraubAdam <weintraubad@gmail.com>
2023-08-01 17:22:41 -0500

    New files for project API middleware and files to begin view

---
+++ src/features/projects/ProjectModel.js
@@ -0,0 +1,11 @@
+export class ProjectModel {
+    constructor(
+      projectID,
+      name,
+      description
+    ) {
+      this.projectID = projectID
+      this.name = name
+      this.description = description
+    }
+  }
